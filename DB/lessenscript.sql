016cf631331305f269fc15c61a543ce27f75893e
Simon <341037@student.mboutrecht.nl>
2025-03-19 09:09:51 +0100

    #

--- DB/lessenscript.sql
+++ DB/lessenscript.sql
@@ -4,41 +4,6 @@ CREATE DATABASE `FitForFun`;
 
 USE `FitForFun`;
 
-CREATE TABLE IF NOT EXISTS lessons (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL
-);
-
-INSERT INTO lessons (name, date, time)
-VALUES
-('Yoga', '2025-03-01', '09:00'),
-('Yin Yoga', '2025-03-01', '12:00'),
-('Meditatie', '2025-03-01', '10:00'),
-('Pilates', '2025-03-01', '11:00');
-
-CREATE TABLE IF NOT EXISTS reservations (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    lesson_id INT,
-    reservation_date DATE NOT NULL,
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    FOREIGN KEY (lesson_id) REFERENCES lessons(id),
-    UNIQUE KEY unique_reservation (date, time)
-);
-
-INSERT INTO reservations 
-(date, time, lesson_id, reservation_date, user_name, user_email) 
-VALUES
-('2025-03-01', '09:00:00', 1, '2025-02-28', 'John Doe', 'john@example.com'),
-('2025-03-01', '12:00:00', 2, '2025-02-28', 'Jane Smith', 'jane@example.com'),
-('2025-03-01', '10:00:00', 3, '2025-02-28', 'Mike Johnson', 'mike@example.com'),
-('2025-03-01', '11:00:00', 4, '2025-02-28', 'Emily Davis', 'emily@example.com');
-
-
 CREATE TABLE Lessen	 
 (
     les_id          INT PRIMARY                         KEY AUTO_INCREMENT,
@@ -111,9 +76,9 @@ VALUES
 ('Vinyasa Flow', '2025-03-12', '18:00:00', 3, 9, 'Niet gestart', 1, 'Dynamische yogales met ademhalingsoefeningen.'),
 ('Yin Yoga', '2025-03-15', '20:00:00', 3, 9, 'Ingepland', 1, 'Ontspannende les met lang aangehouden houdingen.');
 
-DROP DATABASE IF EXISTS `Medewerkers`;
+
 CREATE DATABASE `Medewerkers`;
-USE `Medewerkers`;
+
 
 CREATE TABLE MedewerkersOverzicht
 (
